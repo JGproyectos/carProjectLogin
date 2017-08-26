1db37769e8db774a523e1858dc2e01f689338f4b
Gaspar Hermán <gaspar.herman95@gmail.com>
2017-08-26 18:49:32 -0400

    Agregados scripts de la base de datos

--- turevision/conexion.php
+++ turevision/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexionDB = new mysqli('localhost','root','','mmv');
+$conexionDB = new mysqli('192.168.239.129','root','hola','mmv');
 
 if ($conexionDB -> connect_error) {
   die('error en la conexionDB');
